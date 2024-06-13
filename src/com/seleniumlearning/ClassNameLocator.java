5fa34c02014b0863caed19739cef9d04ee2326a8
AzkaAzhar1 <azka.azhar@yahoo.com>
2024-06-13 20:55:22 +0500

    Second Commit

--- src/com/seleniumlearning/ClassNameLocator.java
+++ src/com/seleniumlearning/ClassNameLocator.java
@@ -11,7 +11,7 @@ public class ClassNameLocator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Open Google Chrome
+		//Open Firefox
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\PC\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
 		
 		//create object of firefox class

6d90b825ba651ce4b9d019fa0e71b042f86b7f8e
AzkaAzhar1 <azka.azhar@yahoo.com>
2024-06-13 20:49:23 +0500

    second commit

---
+++ src/com/seleniumlearning/ClassNameLocator.java
@@ -0,0 +1,29 @@
+package com.seleniumlearning;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ClassNameLocator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Open Google Chrome
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\PC\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+		
+		//create object of firefox class
+		FirefoxDriver driver = new FirefoxDriver();
+		//Open SME Restaurant
+		driver.get("https://resturant.businessholics.com/");
+		
+		//Capture all text boxes of a login page
+	List <WebElement> allTextboxes = driver.findElements(By.className("form-control"));
+	System.out.println("Total text boxes are:" + allTextboxes.size());
+	
+		
+	}
+
+}

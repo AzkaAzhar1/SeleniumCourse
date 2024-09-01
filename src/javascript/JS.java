ef34f2aaa744e57c87638342bc17f5f8b3d99839
AzkaAzhar1 <azka.azhar@yahoo.com>
2024-09-01 11:31:07 +0500

    Create JS.java
    Javascript

---
+++ src/javascript/JS.java
@@ -0,0 +1,45 @@
+package javascript;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class JS {
+
+    public static void main(String[] args) {
+
+        // Set the path to the geckodriver executable
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\PC\\Downloads\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        
+        // Open browser
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        
+        // Open Application
+        driver.get("https://resturant.businessholics.com/login");
+
+        // Explicit wait to ensure element is present
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        // Enter user name
+        WebElement email = driver.findElement(By.id("email"));
+        JavascriptExecutor js = (JavascriptExecutor) driver; // Convert WebDriver to JavascriptExecutor
+        
+        js.executeScript("arguments[0].value='admin@sm.com'", email);
+        
+        // Enter password using Javascript
+        js.executeScript("document.getElementsByName('password')[0].value='admin123'");
+        
+        // Click on login button
+        WebElement loginbutton = driver.findElement(By.className("btn"));
+        js.executeScript("arguments[0].click()", loginbutton);
+        
+        driver.quit(); // Close the browser after actions
+    }
+
+}
